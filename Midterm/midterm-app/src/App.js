e1088176678d76d058c01b391bc30c487a144511
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2023-03-27 10:11:04 -0500

    package json fix

--- Midterm/midterm-app/src/App.js
+++ Midterm/midterm-app/src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
